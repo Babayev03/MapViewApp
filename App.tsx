b7ce051e29b869f6a343840122f3a40cf98e4d19
Babayev03 <ilhambabayev222@gmail.com>
2023-05-16 01:41:00 -0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,86 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {Button, TextInput} from 'react-native-paper';
+import MapView, {Marker} from 'react-native-maps';
+import {Locations} from './src/data/Locations';
+import {Picker} from '@react-native-picker/picker';
+
+const MapScreen = () => {
+  const [cityName, setCityName] = useState<any>('');
+  const [location, setLocation] = useState<any>(null);
+
+  const handleShowLocation = () => {
+    const selectedLocation = Locations.find(
+      location => location.name.toLowerCase() === cityName.toLowerCase(),
+    );
+
+    if (selectedLocation) {
+      setLocation(selectedLocation);
+    } else {
+      setLocation(null);
+    }
+  };
+
+  return (
+    <View style={{flex: 1}}>
+      <Text style={styles.mapText}>Map</Text>
+      <View style={{marginBottom: 10, marginLeft: 15}}>
+        <Picker
+          selectedValue={cityName}
+          onValueChange={(itemValue, itemIndex) => setCityName(itemValue)}>
+          <Picker.Item label="Baku" value="Baku" />
+          <Picker.Item label="Mingachevir" value="Mingachevir" />
+          <Picker.Item label="Sumqayit" value="Sumqayit" />
+          <Picker.Item label="Ganja" value="Ganja" />
+          <Picker.Item label="Lankaran" value="Lankaran" />
+          <Picker.Item label="Neftchala" value="Neftchala" />
+        </Picker>
+      </View>
+
+      <MapView
+        style={styles.map}
+        region={
+          location
+            ? {
+                latitude: location.latitude,
+                longitude: location.longitude,
+                latitudeDelta: 0.815,
+                longitudeDelta: 0.8121,
+              }
+            : null
+        }>
+        {location && (
+          <Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+          />
+        )}
+      </MapView>
+
+      <Button
+        style={{marginTop: 10, marginHorizontal: 15}}
+        mode="contained"
+        onPress={handleShowLocation}>
+        Show Location
+      </Button>
+    </View>
+  );
+};
+
+export default MapScreen;
+
+const styles = StyleSheet.create({
+  map: {
+    height: 400,
+    width: '100%'
+  },
+  mapText: {
+    alignSelf: 'center',
+    marginVertical: 25,
+    fontSize: 22,
+    color: 'white',
+    fontWeight: '600',
+  },
+});
---
+++ src/data/Locations.ts
@@ -0,0 +1,38 @@
+export const Locations = [
+  {
+    id: 1,
+    name: 'Baku',
+    longitude: 49.8671,
+    latitude: 40.4093,
+  },
+  {
+    id: 2,
+    name: 'Ganja',
+    longitude: 46.3606,
+    latitude: 40.6864,
+  },
+  {
+    id: 3,
+    name: 'Sumqayit',
+    longitude: 50.3655,
+    latitude: 40.5897,
+  },
+  {
+    id: 4,
+    name: 'Mingachevir',
+    longitude: 47.0451,
+    latitude: 40.7704,
+  },
+  {
+    id: 5,
+    name: 'Lankaran',
+    longitude: 48.8173,
+    latitude: 38.7543,
+  },
+  {
+    id: 6,
+    name: 'Neftchala',
+    longitude: 49.2478,
+    latitude: 39.3742,
+  },
+];
